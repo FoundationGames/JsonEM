87ecf3dd7a136c407abe8b414e0afd141f3896e6
FoundationGames <43485105+FoundationGames@users.noreply.github.com>
2023-06-27 14:29:10 -0700

    Remove problematic unconditional cancel

--- src/main/java/io/github/foundationgames/jsonem/mixin/EntityModelLoaderMixin.java
+++ src/main/java/io/github/foundationgames/jsonem/mixin/EntityModelLoaderMixin.java
@@ -4,7 +4,6 @@ import io.github.foundationgames.jsonem.util.JsonEntityModelUtil;
 import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.client.render.entity.model.EntityModelLoader;
-import net.minecraft.client.render.entity.model.EntityModels;
 import net.minecraft.resource.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,13 +18,9 @@ import java.util.Map;
 public class EntityModelLoaderMixin {
     @Shadow private Map<EntityModelLayer, TexturedModelData> modelParts;
 
-    @Inject(method = "reload", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "reload", at = @At("TAIL"))
     private void jsonem$loadJsonEntityModels(ResourceManager manager, CallbackInfo ci) {
-        modelParts = new HashMap<>();
-        modelParts.putAll(EntityModels.getModels());
-
-        JsonEntityModelUtil.loadModels(manager, modelParts);
-
-        ci.cancel();
+        this.modelParts = new HashMap<>(this.modelParts);
+        JsonEntityModelUtil.loadModels(manager, this.modelParts);
     }
 }
